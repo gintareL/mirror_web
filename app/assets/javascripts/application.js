9fcf1d7a94aa47979f797ea7c5d293eecc0cf859
gintareL <gintare.lideikyte@gmail.com>
2020-10-10 20:04:14 +0200

    css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery_ujs
 //= require jquery3
 //= require popper
 //= require bootstrap
