aa751f884ffbb53792c32956cac7f7f2136d7417
gintareL <gintare.lideikyte@gmail.com>
2020-10-11 20:34:19 +0200

    add words

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,12 +81,13 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  Rails.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #  logger           = ActiveSupport::Logger.new(STDOUT)
+  #  logger.formatter = config.log_formatter
+  #  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  #end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
