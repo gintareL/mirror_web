ad084f8f4e4588db40d17f02bc9af695e00d402d
gintareL <gintare.lideikyte@gmail.com>
2020-10-11 11:30:21 +0200

    add database

--- app/controllers/mirror_controller.rb
+++ app/controllers/mirror_controller.rb
@@ -1,6 +1,6 @@
 class MirrorController < ApplicationController
   def index
-    #@word = Word.first
+    @word = Word.first
       @weather = get_weather
       unless @weather.nil?
           @max_temp = @weather['main']["temp_max"].to_i
